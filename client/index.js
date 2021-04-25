f3bb7b71f8568d03d617dca34653415ab7c979dd
Lu-Pereira <luci.lv14@gmail.com>
2021-04-25 00:53:46 -0300

    socket  para o client.

--- client/index.js
+++ client/index.js
@@ -1,13 +1,14 @@
 import TerminalController from "./src/terminalController.js";
 import Events from 'events';
 import CliConfig from "./src/cliConfig.js";
+import SocketClient from "./src/socket.js";
 
 // Responsavel por chamar todos os módulos do sistema.
-
 const [nodePath, filePath, ...commands] = process.argv;
-const config = CliConfig.parseArguments(commands)
-console.log('config', config)
+const config = CliConfig.parseArguments(commands);
 const componentEmitter = new Events();
+const SocketClient = new SocketClient(config);
+await SocketClient.initialize();
 
 // const controller = new TerminalController();
 // await controller.initializeTable(componentEmitter);
