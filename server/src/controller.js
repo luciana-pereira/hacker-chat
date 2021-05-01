665a12c1fa0748274ca63e2732ca3967c799044b
Lu-Pereira <luci.lv14@gmail.com>
2021-04-30 23:14:43 -0300

    criação de comandos.

--- server/src/controller.js
+++ server/src/controller.js
@@ -48,6 +48,17 @@ export default class Controller {
         }
     }
 
+    message(socketId, data) {
+        const { userName, roomId } = this.#users.get(socketId);
+        this.broadCast({
+            roomId,
+            socketId,
+            event: constants.event.MESSAGE,
+            message: { userName, message: data },
+            includeCurrentSocket: true,
+        })
+    }
+
     #joinUserOnRoom(roomId, user) {
         const usersOnRoom = this.#rooms.get(roomId) ?? new Map()
         usersOnRoom.set(user.id, user)
@@ -55,10 +66,27 @@ export default class Controller {
 
         return usersOnRoom
     }
+
+    #logoutUser(id, roomId) {
+        this.#users.delete(id)
+        const usersOnRoom = this.#rooms.get(roomId)
+        usersOnRoom.delete(id)
+
+        this.#rooms.set(roomId, usersOnRoom)
+    }
  
     #onSocketClosed(id) {
-        return data => {
-            console.log('onSocketClosed', id)
+        return _ => {
+            const { userName, roomId } = this.#users.get(id);
+            console.log(userName, 'disconectado', id);
+            this.#logoutUser(id, roomId)
+
+            this.broadCast({
+                roomId,
+                message: { id, userName }, 
+                socketId: id,
+                event: constants.event.DISCONNECT_USER,
+            })
         }
     }
 
